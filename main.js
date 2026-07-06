449193f1f06c341d013e6ffa39206b647c43f7b2
mesh-magara <meshackmagara75@gmail.com>
2026-07-06 14:15:17 +0300

    added more functinality to the function addBookToLibrary to include a button that toggles the hasRead property of the book object and updates the text content of the button accordingly.  Also removed the deleteBook function as it was not being used and was redundant.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 
+//book constructor
 function Book(Id, title, author, noOfPages) {
   // the constructor...
   this.Id = Id;
@@ -8,48 +9,94 @@ function Book(Id, title, author, noOfPages) {
   this.noOfPages = noOfPages;
 }
 
+Book.prototype.hasRead = true;
+
+const table = document.querySelector("#book-table");
 function addBookToLibrary(title, author, numberOfPages) {
   // take params, create a book then store it in the array
   const Id = crypto.randomUUID();
-
   //create a new book from the parameters
-  book = {
+  const book = {
     details: new Book(Id, title, author, numberOfPages),
   };
 
+  console.log(book.details.title);
+  const bookRow = document.createElement("tr");
+  const bookId = document.createElement("td");
+  const bookTitle = document.createElement("td");
+  const bookAuthor = document.createElement("td");
+  const bookPages = document.createElement("td");
+  const deleteAction = document.createElement("td");
+  const hasRead = document.createElement("td");
+  bookRow.append(
+    bookId,
+    bookTitle,
+    bookAuthor,
+    bookPages,
+    hasRead,
+    deleteAction,
+  );
+
+  bookId.textContent = book.details.Id;
+  bookTitle.textContent = book.details.title;
+  bookAuthor.textContent = book.details.author;
+  bookPages.textContent = book.details.noOfPages;
+
+  const hasReadBtn = document.createElement("button");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  hasReadBtn.textContent = "Read";
+  hasRead.appendChild(hasReadBtn);
+  deleteAction.appendChild(deleteBtn);
+  table.appendChild(bookRow);
+
   //append the book item in the list
   myLibrary.push(book.details);
-}
 
-const table = document.querySelector("#book-table");
-console.log(table);
-
-//function to append the book on the table
-function displayBook(library) {
-  library.forEach((book) => {
-    const Book = document.createElement("tr");
-    const num = document.createElement("td");
-    const title = document.createElement("td");
-    const author = document.createElement("td");
-    const pages = document.createElement("td");
-
-    Book.append(num, title, author, pages);
-
-    num.textContent = book.Id;
-    title.textContent = book.title;
-    author.textContent = book.author;
-    pages.textContent = book.noOfPages;
-
-    table.appendChild(Book);
+  deleteBtn.addEventListener("click", () => {
+    table.removeChild(bookRow);
   });
+
+  hasReadBtn.addEventListener("click", () => {
+    book.details.hasRead = !book.details.hasRead;
+    hasReadBtn.textContent = book.details.hasRead ? "Unread" : "Read";
+  });
+
+  console.log(myLibrary);
 }
+
+//function to display all  the books currently in the library on a table
 
 const addBtn = document.querySelector("#add-book");
 console.log(addBtn);
 
-addBookToLibrary("Vault is mine", "Paul George", 20);
-displayBook(myLibrary);
+//adding a button for displaying  the form to add a new book
+const form = document.querySelector("#myForm");
+addBtn.addEventListener("click", () => {
+  if (form.style.display === "none") {
+    form.style.display = "block";
+  } else {
+    form.style.display = "none";
+  }
+});
 
-addBookToLibrary("People are scared", "Paul john", 200);
-addBookToLibrary("Vault is mine", "Paul George", 20);
-addBtn.addEventListener("click", displayBook(myLibrary));
+const submit_btn = document.querySelector("#submit-btn");
+const clear_btn = document.querySelector("#clear-form");
+
+//submiting the book details to the table
+submit_btn.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  //getting the values from the form
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#numberOfPages").value;
+  const isReadBtn = document.querySelector("#isRead");
+  addBookToLibrary(title, author, pages);
+  console.log(title, author, pages);
+});
+
+clear_btn.addEventListener("click", function (event) {
+  event.preventDefault();
+  form.style.display = "none";
+});
